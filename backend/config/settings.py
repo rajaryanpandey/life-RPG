ff44153f6518e6f6201e4555e031c958dbb9a755
Raj Aryan Pandey <25LBCS3307@culkomail.in>
2026-09-13 03:47:03 +0530

    vercel.json added

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -98,17 +98,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-if os.environ.get("DATABASE_URL"):
-    DATABASES["default"] = dj_database_url.parse(
-        os.environ["DATABASE_URL"],
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
     )
+}
 
 
 # Password validation
